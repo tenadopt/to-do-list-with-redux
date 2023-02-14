8bfc55bedea536764acc1696eb1e9352af0a7297
Tenad <46803834+tenadopt@users.noreply.github.com>
2023-02-14 22:42:41 +0300

    refactoring

--- src/TodolistWithRedux.tsx
+++ src/TodolistWithRedux.tsx
@@ -52,8 +52,6 @@ export const TodolistWithRedux: FC<TodolistWithReduxPropsType> = ({todolist}) =>
     const onActiveClickHandler = () => dispatch(changeTodolistFilterAC(id, "Active"))
     const onCompletedClickHandler = () => dispatch(changeTodolistFilterAC(id, "Completed"))
 
-
-
     return <div>
         <h3><EditableSpan editableTitle={title} callBack={changeTodolistTitle}/>
             <IconButton onClick={removeTodolist}>
